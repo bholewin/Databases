9c24d5731926c4db62605f31ac0367bbe74894e3
Bryce Holewinski <bholewin@users.noreply.github.com>
2016-07-18 21:39:54 -0700

    Updated #4 & 5

--- Assignment2-selection-5.sql
+++ Assignment2-selection-5.sql
@@ -11,7 +11,7 @@ SELECT film_id FROM `film` WHERE length >= 185 && rental_duration <=3
 SELECT f.film_id, f.title, count(fa.actor_id)
 FROM film f
 INNER JOIN film_actor fa ON fa.film_id = f.film_id
-GROUP BY f.title
+GROUP BY f.title;
 
 #3 Find the average length of films for each language. Your query should return
 #every language even if there is no films in that language. language refers to
@@ -19,20 +19,22 @@ GROUP BY f.title
 SELECT l.name, avg(f.length)
 FROM language l
 LEFT JOIN film f ON f.language_id = l.language_id
-GROUP BY l.name
+GROUP BY l.name;
 
 #4 We want to find out how many of each category of film KEVIN BLOOM has started in
 #so return a table with category.name and the count
 #of the number of films that KEVIN was in which were in that category order by
 #the category name ascending (Your query should return every category even if KEVIN has been in no films in that category).
+
 SELECT c.name, count(f.film_id)
 FROM category c
-INNER JOIN film_category fc ON fc.category_id = c.category_id
+LEFT JOIN film_category fc ON fc.category_id = c.category_id
 INNER JOIN film f ON f.film_id = fc.film_id
 INNER JOIN film_actor fa ON fa.film_id = f.film_id
 INNER JOIN actor a ON a.actor_id = fa.actor_id
 WHERE a.first_name = 'KEVIN' AND a.last_name = 'BLOOM'
-GROUP BY c.name
+GROUP BY c.name;
+
 
 #5 Find the film title of all films which do not feature both SCARLETT DAMON and
 #BEN HARRIS(so you will not list a film if both of these actors have played in that film,
@@ -40,3 +42,14 @@ GROUP BY c.name
 #Order the results by title, descending (use ORDER BY title DESC at the end of the query)
 #Warning, this is a tricky one and while the syntax is all things you know, you have to think oustide
 #the box a bit to figure out how to get a table that shows pairs of actors in movies
+SELECT f.title
+FROM film f
+LEFT OUTER JOIN (SELECT f.title FROM film f
+INNER JOIN film_actor fa ON fa.film_id = f.film_id
+INNER JOIN actor a ON a.actor_id = fa.actor_id
+WHERE a.first_name = 'SCARLETT' AND a.last_name = 'DAMON') sub ON f.title = sub.title
+RIGHT OUTER JOIN (SELECT f.title FROM film f
+INNER JOIN film_actor fa ON fa.film_id = f.film_id
+INNER JOIN actor a ON a.actor_id = fa.actor_id
+WHERE a.first_name = 'BEN' AND a.last_name = 'HARRIS') sub2 ON f.title = sub2.title
+ORDER BY f.title DESC;
